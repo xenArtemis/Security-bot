1ad2143adcb225f67507bf7801a950be78545037
xenArtemis <funniest656@gmail.com>
2024-06-23 10:51:15 +0300

    Added basics
    Added imports + intents

--- code.py
+++ code.py
@@ -1,1 +1,13 @@
+import discord
+from discord.ext import commands
+from datetime import timedelta
 
+intents = discord.Intents.all()
+intents.members = True
+intents.guilds = True
+
+artemis = commands.Bot(command_prefix='+', intents=intents)
+
+@artemis.event
+async def on_ready():
+    print(f'Logged in as {artemis.user}')
